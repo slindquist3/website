988832a234ca396029afa0aea1e0231c2643b2e5
Scott Lindquist <slindquist3@gmail.com>
2025-11-17 20:39:20 -0800

    Scaffold pages

---
+++ pages/watercolors/index.tsx
@@ -0,0 +1,7 @@
+export default function Watercolors() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      This is the watercolor page
+    </div>
+  );
+}
