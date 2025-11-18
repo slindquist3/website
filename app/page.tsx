9af4e6a573ebbbf900bd19f43b38d3f3a0593769
Scott Lindquist <slindquist3@gmail.com>
2025-11-17 20:43:30 -0800

    Add about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <a href="/blog">Blog</a>
-      <a href="/blog">Pixel Art</a>
-      <a href="/blog">Watercolors</a>
+      <ul>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+          <a href="/pixel-art">Pixel Art</a>
+        </li>
+        <li>
+          <a href="/watercolors">Watercolors</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
     </div>
   );
 }
